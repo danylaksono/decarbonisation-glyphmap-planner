c238a3e80d103a06be237977171b91d7433235c2
danylaksono <danylaksono@users.noreply.github.com>
2024-08-23 19:43:39 +0000

    deploy: 8e59b5db41553f81597804f971359d860f074aa0

---
+++ _npm/d3-polygon@1.0.6/_esm.js
@@ -0,0 +1,7 @@
+/**
+ * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
+ * Original file: /npm/d3-polygon@1.0.6/src/index.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+function n(n){for(var r,t=-1,o=n.length,e=n[o-1],l=0;++t<o;)r=e,e=n[t],l+=r[1]*e[0]-r[0]*e[1];return l/2}function r(n){for(var r,t,o=-1,e=n.length,l=0,u=0,f=n[e-1],a=0;++o<e;)r=f,f=n[o],a+=t=r[0]*f[1]-f[0]*r[1],l+=(r[0]+f[0])*t,u+=(r[1]+f[1])*t;return[l/(a*=3),u/a]}function t(n,r){return n[0]-r[0]||n[1]-r[1]}function o(n){for(var r,t,o,e=n.length,l=[0,1],u=2,f=2;f<e;++f){for(;u>1&&(r=n[l[u-2]],t=n[l[u-1]],o=n[f],(t[0]-r[0])*(o[1]-r[1])-(t[1]-r[1])*(o[0]-r[0])<=0);)--u;l[u++]=f}return l.slice(0,u)}function e(n){if((e=n.length)<3)return null;var r,e,l=new Array(e),u=new Array(e);for(r=0;r<e;++r)l[r]=[+n[r][0],+n[r][1],r];for(l.sort(t),r=0;r<e;++r)u[r]=[l[r][0],-l[r][1]];var f=o(l),a=o(u),g=a[0]===f[0],h=a[a.length-1]===f[f.length-1],i=[];for(r=f.length-1;r>=0;--r)i.push(n[l[f[r]][2]]);for(r=+g;r<a.length-h;++r)i.push(n[l[a[r]][2]]);return i}function l(n,r){for(var t,o,e=n.length,l=n[e-1],u=r[0],f=r[1],a=l[0],g=l[1],h=!1,i=0;i<e;++i)t=(l=n[i])[0],(o=l[1])>f!=g>f&&u<(a-t)*(f-o)/(g-o)+t&&(h=!h),a=t,g=o;return h}function u(n){for(var r,t,o=-1,e=n.length,l=n[e-1],u=l[0],f=l[1],a=0;++o<e;)r=u,t=f,r-=u=(l=n[o])[0],t-=f=l[1],a+=Math.sqrt(r*r+t*t);return a}export{n as polygonArea,r as polygonCentroid,l as polygonContains,e as polygonHull,u as polygonLength};export default null;
