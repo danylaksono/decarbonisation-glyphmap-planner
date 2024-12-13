335a918495f0cdc471f101cc8ff4223e5a9b6344
danylaksono <danylaksono@users.noreply.github.com>
2024-12-13 03:27:56 +0000

    deploy: c3e07fc137fdc6b72092345ee595ed6ba67c01e4

--- _npm/apache-arrow@18.1.0/_esm.js
+++ _npm/apache-arrow@18.1.0/_esm.js
@@ -1,6 +1,6 @@
 /**
  * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
- * Original file: /npm/apache-arrow@18.0.0/Arrow.dom.mjs
+ * Original file: /npm/apache-arrow@18.1.0/Arrow.dom.mjs
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
