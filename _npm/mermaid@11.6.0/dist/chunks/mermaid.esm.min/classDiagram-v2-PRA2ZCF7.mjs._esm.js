23e1923017b6b6e8137d3684106fe37551ee7b21
danylaksono <danylaksono@users.noreply.github.com>
2025-06-18 07:46:36 +0000

    deploy: 57437a3d608c7980baf286cb4ff15facf29d663d

--- _npm/mermaid@11.6.0/dist/chunks/mermaid.esm.min/classDiagram-v2-PRA2ZCF7.mjs._esm.js
+++ _npm/mermaid@11.6.0/dist/chunks/mermaid.esm.min/classDiagram-v2-PRA2ZCF7.mjs._esm.js
@@ -1,5 +1,5 @@
 /**
- * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.37.0.
+ * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
  * Original file: /npm/mermaid@11.6.0/dist/chunks/mermaid.esm.min/classDiagram-v2-PRA2ZCF7.mjs
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
